4d897de1f97a7f871a61b7697a92b965f8bdfb4a
Gaji Note <gaji_note@seigaji.info>
2017-01-30 21:12:12 +0900

    応答分サンプル追加

--- resp_if.py
+++ resp_if.py
@@ -16,7 +16,6 @@ def get_source2list(src):
     tmp = i.split(",")
     tmp_list.append(tmp[0])
     tmp_list.append(tmp[1:])
-    # print(tmp_list)
     res_list.append(tmp_list)
     n += 1
   f.close()
@@ -27,26 +26,19 @@ if __name__ == "__main__":
   filename = "resp_list.txt"
   resp_list=get_source2list(filename)
 
-  # print(resp_list)
   num=0
-  # for i in resp_list:
-  #    print(i[1])
 
   res_len=len(resp_list)
-  # print(resp_list[0])
-  # out_w=res_len[0]
-  # print(out_w)
-
+  myname="テストたん"
+  print("あなた :", end="")
   input_line1 = input()
   while (input_line1 is not ""):
     for i in resp_list:
-      # if(input_line1 is resp_list[0]):
-      # print(i)
       match0=re.search(input_line1, i[0])
       if match0:
-        print(match0)
-        print(i[1][0])
-      # else:
-      #   print("なんですって？")
+        lst_size=len(i)
+        word = random.randrange(lst_size+1)
+        print(myname + " :" + i[1][word])
+    print("あなた :", end="")
     input_line1 = input()
-  print("バイバーイ")
+  print(myname + ": バイバーイ")
